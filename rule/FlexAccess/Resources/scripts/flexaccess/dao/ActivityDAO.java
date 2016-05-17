c4a6b12db4ccf70c0b5c39361ded4c0b1891ec0a
medin84 <kalihanovm@gmail.com>
2016-05-17 23:02:51 +0600

    fa like search by rfid

--- rule/FlexAccess/Resources/scripts/flexaccess/dao/ActivityDAO.java
+++ rule/FlexAccess/Resources/scripts/flexaccess/dao/ActivityDAO.java
@@ -1,16 +1,57 @@
 package flexaccess.dao;
 
-import java.util.UUID;
-
+import com.exponentus.dataengine.RuntimeObjUtil;
 import com.exponentus.dataengine.jpa.DAO;
+import com.exponentus.dataengine.jpa.ViewPage;
 import com.exponentus.scripting._Session;
-
 import flexaccess.model.Activity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.UUID;
 
 public class ActivityDAO extends DAO<Activity, UUID> {
 
-	public ActivityDAO(_Session session) {
-		super(Activity.class, session);
-	}
+    public ActivityDAO(_Session session) {
+        super(Activity.class, session);
+    }
 
+    public ViewPage<Activity> findByRFId(String rfid, int pageNum, int pageSize) {
+        EntityManager em = getEntityManagerFactory().createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        try {
+            CriteriaQuery<Activity> cq = cb.createQuery(entityClass);
+            CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
+            Root<Activity> c = cq.from(entityClass);
+            cq.select(c);
+            countCq.select(cb.count(c));
+
+            Predicate condition = cb.like(cb.lower(c.<String>get("rfid")), "%" + rfid + "%");
+            cq.orderBy(cb.asc(c.get("regDate")));
+            cq.where(condition);
+            countCq.where(condition);
+            TypedQuery<Activity> typedQuery = em.createQuery(cq);
+            Query query = em.createQuery(countCq);
+            long count = (long) query.getSingleResult();
+            int maxPage = RuntimeObjUtil.countMaxPage(count, pageSize);
+            if (pageNum == 0) {
+                pageNum = maxPage;
+            }
+            int firstRec = RuntimeObjUtil.calcStartEntry(pageNum, pageSize);
+            typedQuery.setFirstResult(firstRec);
+            typedQuery.setMaxResults(pageSize);
+            List<Activity> result = typedQuery.getResultList();
+
+            ViewPage<Activity> r = new ViewPage<>(result, count, maxPage, pageNum);
+            return r;
+        } finally {
+            em.close();
+        }
+    }
 }
--- rule/FlexAccess/Resources/scripts/flexaccess/page/view/ActivityView.java
+++ rule/FlexAccess/Resources/scripts/flexaccess/page/view/ActivityView.java
@@ -1,18 +1,35 @@
 package flexaccess.page.view;
 
+import com.exponentus.dataengine.jpa.ViewPage;
+import com.exponentus.scripting._POJOListWrapper;
 import com.exponentus.scripting._Session;
 import com.exponentus.scripting._WebFormData;
+import com.exponentus.scripting.event._DoPage;
 import flexaccess.dao.ActivityDAO;
+import flexaccess.model.Activity;
 
-public class ActivityView extends AbstractFlexAccessView {
+import java.util.Date;
+
+public class ActivityView extends _DoPage {
 
     @Override
     public void doGET(_Session session, _WebFormData formData) {
-        addContent(getViewPage(new ActivityDAO(session), formData));
-    }
+        ActivityDAO activityDAO = new ActivityDAO(session);
 
-    @Override
-    public void doDELETE(_Session session, _WebFormData formData) {
+        int pageNum = formData.getNumberValueSilently("page", 1);
+        int pageSize = session.getPageSize();
 
+        String docid = formData.getValueSilently("docid");
+        String rfid = formData.getValueSilently("rfid").toLowerCase();
+        Date timeFrom = formData.getDateSilently("timeFrom");
+        Date timeTo = formData.getDateSilently("timeTo");
+        Date timeInterval[] = {timeFrom, timeTo};
+
+        if (!rfid.isEmpty()) {
+            ViewPage<Activity> result = activityDAO.findByRFId(rfid, pageNum, pageSize);
+            addContent(new _POJOListWrapper<>(result.getResult(), result.getMaxPage(), result.getCount(), result.getPageNum(), session));
+        } else {
+            addContent(getViewPage(activityDAO, formData));
+        }
     }
 }
