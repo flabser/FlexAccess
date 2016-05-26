42f6c983b09500b5dae67463bfa6db1ff8af72a8
k-zon_000 <k-zon_000@KayraNote>
2016-05-26 09:26:59 +0300

    #

--- rule/Stock/Resources/scripts/stock/model/Unit.java
+++ rule/Stock/Resources/scripts/stock/model/Unit.java
@@ -8,33 +8,26 @@ import javax.persistence.Table;
 
 import com.exponentus.dataengine.jpa.SecureAppEntity;
 
-import reference.model.UnitType;
+//import reference.model.UnitType;
 
 @Entity
 @Table(name = "units")
 @NamedQuery(name = "Unit.findAll", query = "SELECT m FROM Unit AS m ORDER BY m.regDate")
 public class Unit extends SecureAppEntity<UUID> {
 
-	private UnitType cat;
-
-	private String name;
-
-	private String rfid;
-
-	public String getRfid() {
-		return rfid;
-	}
-
-	public void setRfid(String rfid) {
-		this.rfid = rfid;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+	/*
+	 * private UnitType cat;
+	 * 
+	 * private String name;
+	 * 
+	 * private String rfid;
+	 * 
+	 * public String getRfid() { return rfid; }
+	 * 
+	 * public void setRfid(String rfid) { this.rfid = rfid; }
+	 * 
+	 * public String getName() { return name; }
+	 * 
+	 * public void setName(String name) { this.name = name; }
+	 */
 }
