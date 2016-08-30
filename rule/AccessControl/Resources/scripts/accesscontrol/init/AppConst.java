27ba7f2ae69e6e45faa2ad2afd917145a853feb9
k-zon_000 <k-zon_000@KayraNote>
2016-08-30 22:48:52 +0300

    #

--- rule/AccessControl/Resources/scripts/accesscontrol/init/AppConst.java
+++ rule/AccessControl/Resources/scripts/accesscontrol/init/AppConst.java
@@ -5,6 +5,6 @@ public class AppConst {
 	public static String NAME = "AccessControl";
 	public static String NAME_ENG = "Access Control";
 	public static String NAME_RUS = "Контроль доступа";
-	public static String DEFAULT_URL = "index.html";
+	public static String DEFAULT_PAGE = "activity-view";
 	public static String FT_INDEX_SCOPE = "";
 }
